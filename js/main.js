b799c817715ed252d46cb671ec99346785f45ef3
Nurul Arifin <77090543+nurularifin837@users.noreply.github.com>
2022-06-18 13:50:51 +0700

    Make an Carousel index.html and also put some code on style and min.js

--- js/main.js
+++ js/main.js
@@ -16,4 +16,22 @@
     // Initiate the wowjs
     new WOW().init();
 
+    //Fixed Navbar
+    $(window).scroll(function () {
+        if ($(window).width() < 992) {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-white shadow');
+            } else {
+                $('.fixed-top').removeClass('bg-white shadow');
+            }            
+        } else {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-white shadow').css('top', -45);
+            } else {
+                $('.fixed-top').removeClass('bg-white shadow').css('top', 0);
+            }
+        }
+
+    });
+
 })(jQuery);
